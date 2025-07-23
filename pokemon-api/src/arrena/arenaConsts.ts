9f21a1a7256f6378d1762aaed9b4c713b34718d7
Sharon-Shechter-HLS <sharons@moveohls.com>
2025-07-23 13:58:17 +0300

    feat: added attack logic

--- pokemon-api/src/arrena/arenaConsts.ts
+++ pokemon-api/src/arrena/arenaConsts.ts
@@ -14,3 +14,10 @@ export const BASE_CATCH_CHANCE = 0.1;
 export const LOW_LIFE_THRESHOLD_PERCENTAGE = 0.25; 
 export const MAX_CATCH_ATTEMPTS = 3; 
 export const LOW_LIFE_CATCH_CHANCE = 0.25; 
+
+//atack Logic.ts
+export const BASE_DAMAGE_MULTIPLIER = 0.25;
+export const RANDOM_DAMAGE_MULTIPLIER = 0.05;
+export const MIN_DAMAGE = 1;
+export const DAMAGE_CHANCE_THRESHOLD = 0.2;
+export const DAMAGE_CALCULATION_DELAY = 200; 
